6cbe11bc3801f00580297bd5ad929445272074d9
Matt Raible <matt.raible@okta.com>
2018-12-19 01:52:56 -0700

    No dist

--- dist/index.d.ts
+++
@@ -1,6 +0,0 @@
-import { Rule } from '@angular-devkit/schematics';
-export interface OktaOptions {
-    clientId: string;
-    issuer: string;
-}
-export default function (options: OktaOptions): Rule;
