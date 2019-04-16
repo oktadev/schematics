bd1638a3329f9171f7c029eaf3230537f337a22e
Matt Raible <matt.raible@okta.com>
2019-04-16 08:18:28 -0600

    Upgrade Angular, React, and Vue SDKs

--- src/add-auth/angular/src/app/app.module.ts
+++ src/add-auth/angular/src/app/app.module.ts
@@ -4,10 +4,10 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
-import { OktaAuthModule } from '@okta/okta-angular';
+import { OKTA_CONFIG, OktaAuthModule } from '@okta/okta-angular';
 import { AuthInterceptor } from './shared/okta/auth.interceptor';
 
-const config = {
+const oktaConfig = {
   issuer: '<%= issuer %>',
   redirectUri: window.location.origin + '/implicit/callback',
   clientId: '<%= clientId %>'
@@ -22,9 +22,12 @@ const config = {
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    OktaAuthModule.initAuth(config)
+    OktaAuthModule
   ],
-  providers: [{provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true}],
+  providers: [
+    { provide: OKTA_CONFIG, useValue: oktaConfig },
+    { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
