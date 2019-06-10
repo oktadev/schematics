c348d3efb90086e6d3c0fe0b8c37479dc65cbc21
Matt Raible <matt.raible@okta.com>
2019-06-10 15:48:25 -0600

    Polishing

--- src/add-auth/ionic/angular/src/app/auth/auth.service.ts
+++ src/add-auth/ionic/angular/src/app/auth/auth.service.ts
@@ -1,6 +1,5 @@
 import { Platform } from '@ionic/angular';
 import { Injectable, NgZone } from '@angular/core';
-<% if (configUri) { %>import { HttpClient } from '@angular/common/http';<% } %>
 import { IonicAuth, IonicAuthorizationRequestHandler } from 'ionic-appauth';
 import { BrowserService } from './browser.service';
 import { CordovaRequestorService } from './cordova-requestor.service';
@@ -10,8 +9,7 @@ import { RequestorService } from './requestor.service';
 <% if (platform === 'capacitor') { %>import { Plugins, AppUrlOpen } from '@capacitor/core';
 
 const { App } = Plugins;<% } %>
-<% if (configUri) { %>
-interface AuthConfig {
+<% if (configUri) { %>interface AuthConfig {
   issuer: string;
   clientId: string;
 }<% } %>
@@ -23,7 +21,7 @@ export class AuthService extends IonicAuth {
 
   constructor(requestor: RequestorService, cordovaRequestor: CordovaRequestorService,
               storage: StorageService, secureStorage: SecureStorageService, browser: BrowserService,
-              private platform: Platform, private ngZone: NgZone<% if (configUri) { %>, private http: HttpClient<% } %>) {<% if (platform === 'cordova') { %>
+              private platform: Platform, private ngZone: NgZone) {<% if (platform === 'cordova') { %>
       super((platform.is('cordova')) ? browser : undefined,
         (platform.is('cordova')) ? secureStorage : storage,
         (platform.is('cordova')) ? cordovaRequestor : requestor);<% } else { %>
@@ -61,49 +59,51 @@ export class AuthService extends IonicAuth {
     return <% if (platform === 'cordova') { %>this.platform.is('cordova')<% } else { %>this.platform.is('mobile') && !this.platform.is('mobileweb')<% } %>;
   }
 
-  private addConfig() {
+  private async addConfig() {
     const scopes = 'openid profile offline_access';
-  <% if (configUri) { %>
-    const AUTH_CONFIG: string = '<%= configUri %>';
-    if (localStorage.getItem(AUTH_CONFIG)) {
-      this.authConfig = JSON.parse(localStorage.getItem(AUTH_CONFIG));
-      localStorage.removeItem(AUTH_CONFIG);
+    const redirectUri = this.onDevice() ? '<%= packageName %>:/callback' : 'http://localhost:8100/implicit/callback';
+    const logoutRedirectUri = this.onDevice() ? '<%= packageName %>:/logout' : 'http://localhost:8100/implicit/logout';
+    <% if (configUri) { %>const AUTH_CONFIG_URI: string = '<%= configUri %>';
+
+    if (await this.storage.getItem(AUTH_CONFIG_URI)) {
+      this.authConfig = JSON.parse(await this.storage.getItem(AUTH_CONFIG_URI));
+      await this.storage.removeItem(AUTH_CONFIG_URI);
     } else {
       // try to get the oauth settings from the server
-      this.http.get(AUTH_CONFIG).subscribe((data: any) => {
+      this.requestor.xhr({method: 'GET', url: AUTH_CONFIG_URI}).then(async (data: any) => {
         this.authConfig = {
           identity_client: data.clientId,
           identity_server: data.issuer,
-          // todo: change localhost to production URL before deploying
-          redirect_url: this.onDevice() ? '<%= packageName %>:/callback' : 'http://localhost:8100/implicit/callback',
+          redirect_url: redirectUri,
+          end_session_redirect_url: logoutRedirectUri,
           scopes,
-          usePkce: true,
-          response_type: 'code',
-          end_session_redirect_url: this.onDevice() ? '<%= packageName %>:/logout' : 'http://localhost:8100/implicit/logout'
+          usePkce: true
         };
-        localStorage.setItem(AUTH_CONFIG, JSON.stringify(this.authConfig));
+        await this.storage.setItem(AUTH_CONFIG_URI, JSON.stringify(this.authConfig));
+      }, error => {
+        console.error('ERROR fetching authentication information, defaulting to Keycloak settings');
+        console.error(error);
+        this.authConfig = {
+          identity_client: 'web_app',
+          identity_server: 'http://localhost:9080/auth/realms/jhipster',
+          redirect_url: redirectUri,
+          end_session_redirect_url: logoutRedirectUri,
+          scopes,
+          usePkce: true
+        };
       });
-  <% } else { %>
-    const clientId = '<%= clientId %>';
+    }<% } else { %>const clientId = '<%= clientId %>';
     const issuer = '<%= issuer %>';
     const authConfig: any = {
       identity_client: clientId,
       identity_server: issuer,
+      redirect_url: redirectUri,
+      end_session_redirect_url: logoutRedirectUri,
       scopes,
       usePkce: true,
     };
 
-    if (this.onDevice()) {
-      authConfig.redirect_url = 'com.oktapreview.dev-737523:/callback';
-      authConfig.end_session_redirect_url = 'com.oktapreview.dev-737523:/logout';
-    } else {
-      authConfig.redirect_url = 'http://localhost:8100/implicit/callback';
-      authConfig.end_session_redirect_url = 'http://localhost:8100/implicit/logout';
-    }
-
-    this.authConfig = {...authConfig};
-  <% } %>
-    }
+    this.authConfig = {...authConfig};<% } %>
   }
 
   private handleCallback(callbackUrl: string): void {
