0e776d33b6487433fe48257ebb6980d4085f7a90
Matt Raible <matt.raible@okta.com>
2019-09-23 14:54:43 -0600

    Fix Angular test

--- src/add-auth/angular/src/app/app.component.spec.ts
+++ src/add-auth/angular/src/app/app.component.spec.ts
@@ -32,10 +32,10 @@ describe('AppComponent', () => {
     expect(app.title).toEqual('<%= project %>');
   });
 
-  it('should render title in a h1 tag', () => {
+  it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to <%= project %>!');
+    expect(compiled.querySelector('.content span').textContent).toContain('<%= project %> app is running!');
   });
 });
