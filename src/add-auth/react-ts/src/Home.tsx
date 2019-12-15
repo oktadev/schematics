eda775fd64ba2eaefc375c13b4474ac08e35d179
Matt Raible <matt.raible@okta.com>
2019-12-15 16:10:29 -0700

    Add link to React + TypeScript template

--- src/add-auth/react-ts/src/Home.tsx
+++ src/add-auth/react-ts/src/Home.tsx
@@ -68,6 +68,9 @@ export default withAuth(class Home extends Component<HomeProps, HomeState> {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo"/>
           <h1 className="App-title">Welcome to React</h1>
+          <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
+            Learn React
+          </a>
           {body}
         </header>
       </div>
