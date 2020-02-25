d5a0776a4ed39a18999b19a6be6fe625524779bb
Matt Raible <matt.raible@okta.com>
2020-02-24 18:25:31 -0700

    Upgrade to Ionic 5 and ionic-appauth 0.4.6

--- src/add-auth/ionic/angular/src/app/auth/auth-http.service.ts
+++ src/add-auth/ionic/angular/src/app/auth/auth-http.service.ts
@@ -1,27 +1,13 @@
-import { Platform } from '@ionic/angular';
 import { Injectable } from '@angular/core';
 import { Requestor, TokenResponse } from '@openid/appauth';
-
 import { AuthService } from './auth.service';
-import { CordovaRequestor } from 'ionic-appauth/lib/cordova';
-import { RequestorService } from './requestor.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthHttpService {
 
-  requestor: Requestor;
-
-  constructor(
-    angularRequestor: RequestorService,
-    cordovaRequestor: CordovaRequestor,
-    platform: Platform,
-    private auth: AuthService
-  ) {
-    this.requestor = platform.is('cordova') ? cordovaRequestor : angularRequestor;
-
-  }
+  constructor(private requestor: Requestor, private auth: AuthService) { }
 
   public async request<T>(method: 'GET' | 'POST' | 'PUT' | 'DELETE', url: string, body?: any) {
     const token: TokenResponse = await this.auth.getValidToken();
@@ -35,9 +21,8 @@ export class AuthHttpService {
 
   private addHeaders(token) {
     return (token) ? {
-      'Authorization': `${token.tokenType} ${token.accessToken}`,
+      'Authorization': `${(token.tokenType === 'bearer') ? 'Bearer' : token.tokenType} ${token.accessToken}`,
       'Content-Type': 'application/json'
     } : {};
-
   }
 }
--- src/add-auth/ionic/angular/src/app/auth/auth.module.ts
+++ src/add-auth/ionic/angular/src/app/auth/auth.module.ts
@@ -1,22 +1,33 @@
+import { Requestor, StorageBackend } from '@openid/appauth';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-import { AuthService } from './auth.service';
-import { AuthHttpService } from './auth-http.service';
-import { AuthGuardService } from './auth-guard.service';
-import { StorageService } from './storage.service';
-import { RequestorService } from './requestor.service';
+import { Platform } from '@ionic/angular';
+
+import { HttpClient } from '@angular/common/http';
+import { browserFactory, httpFactory, storageFactory } from './factories';
+import { Browser } from 'ionic-appauth';
 
 @NgModule({
   imports: [
     CommonModule
   ],
   providers: [
-    RequestorService,
-    StorageService,
-    AuthGuardService,
-    AuthHttpService,
-    AuthService
+    {
+      provide: StorageBackend,
+      useFactory: storageFactory,
+      deps: [Platform]
+    },
+    {
+      provide: Requestor,
+      useFactory: httpFactory,
+      deps: [Platform, HttpClient]
+    },
+    {
+      provide: Browser,
+      useFactory: browserFactory,
+      deps: [Platform]
+    }
   ]
 })
 export class AuthModule {
