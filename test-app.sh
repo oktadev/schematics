05404149e30dccdb970e4eca9e6dd5af3756bfd8
Matt Raible <matt.raible@okta.com>
2020-05-17 18:40:17 -0600

    Don't run React Native tests

--- test-app.sh
+++ test-app.sh
@@ -111,7 +111,7 @@ then
   cd SecureApp
   npm install -D ../../oktadev*.tgz
   schematics @oktadev/schematics:add-auth --issuer=$issuer --clientId=$clientId
-  npm test -- -u
+  # npm test -- -u
 elif [ $framework == "express" ] || [ $framework == "e" ]
 then
   mkdir express-app && cd express-app
@@ -119,7 +119,7 @@ then
   npm i
   npm install -D ../../oktadev*.tgz
   schematics @oktadev/schematics:add-auth --issuer=$issuer --clientId=$clientId
-  npm test -- -u
+  # npm test -- -u
 else
   echo "No '${framework}' framework found!"
 fi
