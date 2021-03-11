b935296c2dc5157c625dbdf78bbb5b807096a55c
Matt Raible <matt.raible@okta.com>
2021-03-11 11:46:38 -0700

    Start integrating Okta React v5

--- src/add-auth/react/src/App.js
+++ src/add-auth/react/src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
-import { OktaAuth } from '@okta/okta-auth-js';
+import { BrowserRouter as Router, Route, useHistory } from 'react-router-dom';
+import { OktaAuth, toRelativeUrl } from '@okta/okta-auth-js';
 import { LoginCallback, Security } from '@okta/okta-react';
 import Home from './Home';
 
@@ -12,10 +12,15 @@ const oktaAuth = new OktaAuth({
 
 class App extends Component {
 
+  const history = useHistory();
+  const restoreOriginalUri = async (_oktaAuth, originalUri) => {
+    history.replace(toRelativeUrl(originalUri, window.location.origin));
+  };
+
   render() {
     return (
       <Router>
-        <Security oktaAuth={oktaAuth}>
+        <Security oktaAuth={oktaAuth} restoreOriginalUri={restoreOriginalUri}>
           <Route path="/" exact={true} component={Home}/>
           <Route path="/callback" component={LoginCallback}/>
         </Security>
