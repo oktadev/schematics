88fba5d2b81e490056921f578c7304f6f4b100e4
Matt Raible <matt.raible@okta.com>
2021-03-15 12:45:08 -0600

    Fix code for Okta React v5

--- src/add-auth/react/src/App.js
+++ src/add-auth/react/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route, useHistory } from 'react-router-dom';
+import { BrowserRouter as Router, Route, withRouter } from 'react-router-dom';
 import { OktaAuth, toRelativeUrl } from '@okta/okta-auth-js';
 import { LoginCallback, Security } from '@okta/okta-react';
 import Home from './Home';
@@ -12,21 +12,29 @@ const oktaAuth = new OktaAuth({
 
 class App extends Component {
 
-  const history = useHistory();
-  const restoreOriginalUri = async (_oktaAuth, originalUri) => {
-    history.replace(toRelativeUrl(originalUri, window.location.origin));
-  };
+  constructor(props) {
+    super(props);
+    this.restoreOriginalUri = async (_oktaAuth, originalUri) => {
+      props.history.replace(toRelativeUrl(originalUri, window.location.origin));
+    };
+  }
 
   render() {
     return (
-      <Router>
-        <Security oktaAuth={oktaAuth} restoreOriginalUri={restoreOriginalUri}>
-          <Route path="/" exact={true} component={Home}/>
-          <Route path="/callback" component={LoginCallback}/>
-        </Security>
-      </Router>
+      <Security oktaAuth={oktaAuth} restoreOriginalUri={this.restoreOriginalUri}>
+        <Route path="/" exact={true} component={Home}/>
+        <Route path="/callback" component={LoginCallback}/>
+      </Security>
     );
   }
 }
 
-export default App;
+const AppWithRouterAccess = withRouter(App);
+
+class RouterApp extends Component {
+  render() {
+    return (<Router><AppWithRouterAccess/></Router>);
+  }
+}
+
+export default RouterApp;
