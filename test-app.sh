d7432f8562fb318a9e514ca72a50d1c1c32ad3d4
Matt Raible <matt.raible@okta.com>
2021-03-24 10:09:21 -0600

    Revert Okta issuer and client ID

--- test-app.sh
+++ test-app.sh
@@ -4,8 +4,8 @@
 set -e
 
 framework="$1"
-issuer="https://dev-9323263.okta.com/oauth2/default"
-clientId="0oacybunw0BvMDbA55d6"
+issuer="https://dev-5597275.okta.com/oauth2/default"
+clientId="0oa3yrnqgg2UI9Lp35d6"
 
 # build and package this project
 rm -f *.tgz
