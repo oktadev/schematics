7774d41c03904c462d832499be471713713150ae
Matt Raible <matt.raible@okta.com>
2021-04-21 14:56:22 -0600

    Polishing

--- src/add-auth/vue3-ts/src/App.vue
+++ src/add-auth/vue3-ts/src/App.vue
@@ -11,8 +11,8 @@
   <router-view/>
 </template>
 
-<script>
-import { defineComponent } from 'vue'
+<script lang="ts">
+import { defineComponent } from 'vue';
 
 export default defineComponent({
   name: 'app',
