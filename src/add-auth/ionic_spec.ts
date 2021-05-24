61d44d4d27fb8fab07f091bbdf4b799f9a525228
Matt Raible <matt.raible@okta.com>
2021-05-24 11:33:13 -0600

    Revert removal of npm-shrinkwrap

--- src/add-auth/ionic_spec.ts
+++ src/add-auth/ionic_spec.ts
@@ -127,8 +127,9 @@ describe('OktaDev Schematics: Ionic/Angular', () => {
     const runner = new SchematicTestRunner('schematics', collectionPath);
     await runner.runSchematicAsync('add-auth', capacitorOptions, tree).toPromise();
 
-    expect(tree.files.length).toEqual(28);
+    expect(tree.files.length).toEqual(29);
     expect(tree.files.sort()).toEqual([
+      '/npm-shrinkwrap.json',
       '/package.json',
       '/src/app/app-routing.module.ts',
       '/src/app/app.component.spec.ts',
@@ -186,8 +187,8 @@ describe('OktaDev Schematics: Ionic/Angular', () => {
     const runner = new SchematicTestRunner('schematics', collectionPath);
     await runner.runSchematicAsync('add-auth', jhipsterOptions, tree).toPromise();
 
-    expect(tree.files.length).toEqual(29);
-    expect(tree.files.sort()[0]).toMatch('package.json');
+    expect(tree.files.length).toEqual(30);
+    expect(tree.files.sort()[0]).toMatch('npm-shrinkwrap.json');
 
     const authConfig = tree.readContent('src/app/auth/auth-config.service.ts');
     expect(authConfig).toContain('${environment.apiUrl}/auth-info');
