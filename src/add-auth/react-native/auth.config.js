d1e4338d507d37475e367e1e661c82f32cc808a5
Matt Raible <matt.raible@okta.com>
2021-10-01 11:01:17 -0600

    Upgrade to okta-react-native v2.1.1 (#683)

--- src/add-auth/react-native/auth.config.js
+++ src/add-auth/react-native/auth.config.js
@@ -2,7 +2,7 @@ export default {
   oidc: {
     clientId: '<%= clientId %>',
     redirectUri: '<%= packageName %>:/callback',
-    endSessionRedirectUri: '<%= packageName %>:/callback',
+    endSessionRedirectUri: '<%= packageName %>:/logout',
     discoveryUri: '<%= issuer %>',
     scopes: ['openid', 'profile', 'offline_access'],
     requireHardwareBackedKeyStore: false,
