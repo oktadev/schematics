b348d0f1ef2bebdaecd48823cee4f921d9da7753
Matt Raible <matt.raible@okta.com>
2022-02-04 16:21:57 -0700

    Update tests for Angular 13

--- src/add-auth/angular/src/app/app.component.spec.ts
+++ src/add-auth/angular/src/app/app.component.spec.ts
@@ -27,20 +27,20 @@ describe('AppComponent', () => {
 
   it('should create the app', () => {
     const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
+    const app = fixture.componentInstance;
     expect(app).toBeTruthy();
   });
 
   it(`should have as title '<%= project %>'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
+    const app = fixture.componentInstance;
     expect(app.title).toEqual('<%= project %>');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('<%= project %> app is running!');
+    const compiled = fixture.nativeElement as HTMLElement;
+    expect(compiled.querySelector('.content span')?.textContent).toContain('<%= project %> app is running!');
   });
 });
