258b7302b521a5a7a5a38510ad13c04de604d47c
Matt Raible <matt.raible@okta.com>
2022-02-07 13:28:07 -0700

    Fix injection in Angular interceptor (#782)

--- src/add-auth/angular/src/app/shared/okta/auth.interceptor.ts
+++ src/add-auth/angular/src/app/shared/okta/auth.interceptor.ts
@@ -1,12 +1,13 @@
 import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { Injectable } from '@angular/core';
+import { Inject, Injectable } from '@angular/core';
+import { OKTA_AUTH } from '@okta/okta-angular';
 import { OktaAuth } from '@okta/okta-auth-js';
 
 @Injectable()
 export class AuthInterceptor implements HttpInterceptor {
 
-  constructor(private oktaAuth: OktaAuth) {
+  constructor(@Inject(OKTA_AUTH) private oktaAuth: OktaAuth) {
   }
 
   intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
