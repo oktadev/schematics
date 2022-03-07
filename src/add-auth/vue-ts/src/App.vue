cb8ecfb28f69913050120587f784adfd030a5632
Matt Raible <matt.raible@okta.com>
2022-03-07 09:23:28 -0700

    Fix Vue 3 and upgrade to Auth JS 6.2

--- src/add-auth/vue-ts/src/App.vue
+++ src/add-auth/vue-ts/src/App.vue
@@ -1,12 +1,12 @@
 <template>
-  <div id="app">
-    <div id="nav">
+  <nav id="app">
+    <nav>
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
       <template v-if="authState.isAuthenticated"> |
         <!-- router links that require authentication -->
       </template>
-    </div>
+    </nav>
     <button v-if="authState.isAuthenticated" v-on:click="logout" id="logout">Logout</button>
     <button v-else v-on:click="login" id="login">Login</button>
     <router-view/>
@@ -31,22 +31,23 @@ export default Vue.extend({
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
 }
-#nav {
+
+nav {
   padding: 30px;
 }
 
-#nav a {
+nav a {
   font-weight: bold;
   color: #2c3e50;
 }
 
-#nav a.router-link-exact-active {
+nav a.router-link-exact-active {
   color: #42b983;
 }
 </style>
